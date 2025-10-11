3e123518397c045afc0b0a97938b593cc2bf5074
Shuhurat <shururatfariha@gmail.com>
2025-10-12 02:30:50 +0600

    Final updates from new branch

--- obj/Debug/net8.0/ResQPaw.AssemblyInfo.cs
+++ obj/Debug/net8.0/ResQPaw.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResQPaw")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e31eb952d26f2e4d152fac967fff875c1fb0b87a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+247536cae991d16d75f6bcf502ab30aa59901f5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResQPaw")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResQPaw")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
