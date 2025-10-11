f951d4b84326b8ca8ad6fc1417881a9038c73425
Shuhurat <shururatfariha@gmail.com>
2025-10-12 03:03:18 +0600

    Add source code changes

--- obj/Debug/net8.0/ResQPaw.AssemblyInfo.cs
+++ obj/Debug/net8.0/ResQPaw.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ResQPaw")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e123518397c045afc0b0a97938b593cc2bf5074")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c06c0e1ad1ed683d0b14df6846643cc7e575b0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ResQPaw")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ResQPaw")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
