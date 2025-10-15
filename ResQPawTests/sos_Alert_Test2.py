66e7e067f89da3dc18d9d31a175f7e451ebd7a3e
Shuhurat <shururatfariha@gmail.com>
2025-10-15 23:30:50 +0600

    login test and sos submission test

---
+++ ResQPawTests/sos_Alert_Test2.py
@@ -0,0 +1,73 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+
+import time
+
+
+driver = webdriver.Chrome()  
+driver.maximize_window()
+
+try:
+
+    driver.get("http://localhost:5223/Identity/Account/Login")
+    
+    username = driver.find_element(By.NAME, "Input.Email")
+    password = driver.find_element(By.NAME, "Input.Password")
+
+    
+    username.send_keys("user25@gmail.com")
+    password.send_keys("h2ZKZPbd35m@kkH")
+
+   
+    driver.find_element(By.ID, "login-submit").click()
+
+    
+    time.sleep(3)
+
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "customerDashboardLink"))
+    )
+    print("✅ Login successful!")
+
+    driver.find_element(By.ID, "sosBtn").click()
+    driver.find_element(By.ID, "callEmergency").click()
+
+    WebDriverWait(driver, 5).until(
+        EC.url_to_be("http://localhost:5223/SOS/Send")
+    )
+
+    driver.find_element(By.NAME, "EmergencyType").send_keys("Injury")
+    driver.find_element(By.NAME, "AnimalType").send_keys("Dog")
+    driver.find_element(By.NAME, "AnimalCondition").send_keys("Bleeding")
+    driver.find_element(By.NAME, "Description").send_keys("Leg injured, needs urgent care")
+    driver.find_element(By.NAME, "Address").send_keys("Banani, Dhaka")
+    driver.find_element(By.NAME, "ReporterName").send_keys("John Doe")
+    driver.find_element(By.NAME, "ReporterPhone").send_keys("01712345678")
+    driver.find_element(By.NAME, "ReporterEmail").send_keys("johndoe@gmail.com")
+
+    print("submitted all form fields.")
+
+    driver.find_element(By.ID, "submitBtn").click()
+
+   
+
+    try:
+        success_message = WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.ID, "successMessage"))
+        )
+        print("SOS Alert submitted successfully!")
+        print("Message:", success_message.text)
+    except TimeoutException:
+        print(" Success message not found. Submission may have failed.")
+        driver.save_screenshot("sos_submission_failed.png")
+
+except NoSuchElementException as e:
+    print(" Element not found:", e)
+    driver.save_screenshot("sos_error.png")
+
+finally:
+    driver.quit()
